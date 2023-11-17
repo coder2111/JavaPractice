1edfe8f10f7b9e0c908aeade2a31909c157db4f6
ravij <ravij@192.168.1.101>
2023-11-17 16:54:34 +0530

    changes

--- Testing2/src/ss/ReverseArray.java
+++ Testing2/src/ss/ReverseArray.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class ReverseArray {
 public static void main(String args[]) {
 	int arr[]= {1,2,3,4,5};
-	int rev[] = new int[arr.length];
 	int len=arr.length;
+	int rev[] = new int[len];
 	for(int i=0;i<len;i++) {
 		rev[i]=arr[len-1-i];
 	}
